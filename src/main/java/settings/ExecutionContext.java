9e469aae8f2b1c8f8de6c68397c82e2e9ac9db82
Eli Josué Poel Sandoval <eli.sandoval@appdirect.com>
2020-08-05 17:25:25 -0300

    execution context

---
+++ src/main/java/settings/ExecutionContext.java
@@ -0,0 +1,32 @@
+package main.java.settings;
+
+import java.util.Objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExecutionContext {
+	private static ExecutionContext instance;
+	private WebDriver driver;
+
+	private ExecutionContext() {
+		System.setProperty("webdriver.chrome.driver", "/Users/eli.sandoval/Downloads/chromedriver");
+		this.driver = new ChromeDriver();
+	}
+
+	public static ExecutionContext getInstance() {
+		if (Objects.isNull(instance)) {
+			instance = new ExecutionContext();
+		}
+		return instance;
+	}
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+	public void quit() {
+		driver.quit();
+		instance = null;
+	}
+}
