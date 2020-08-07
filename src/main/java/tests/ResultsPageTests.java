1231c139ed27e76563ae1d6f82c9c719d4161920
Eli Josué Poel Sandoval <eli.sandoval@appdirect.com>
2020-08-07 17:12:27 -0300

    Added methods to test class

--- src/main/java/tests/ResultsPageTests.java
+++ src/main/java/tests/ResultsPageTests.java
@@ -3,11 +3,17 @@ package main.java.tests;
 import org.testng.annotations.Test;
 
 import main.java.pages.HomePage;
+import main.java.pages.ProductPage;
 
 public class ResultsPageTests extends AbstractTest {
+	private ProductPage productPage;
 
 	@Test
-	public void firstDraftTest() {
-		HomePage homePage = new HomePage();
+	public void checkSecondProductQuantity() throws InterruptedException {
+		productPage = new HomePage()
+			.search("Iphone")
+			.clickNextPage()
+			.clickSecondProduct()
+			.checkQuantity(1);
 	}
 }
