b7749fdcf4b26a96f06fa2afe64a3a2611df9801
Eli Josué Poel Sandoval <eli.sandoval@appdirect.com>
2020-08-07 17:10:30 -0300

    Added webelements and search method to home page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,13 +1,44 @@
 package main.java.pages;
 
-import org.apache.commons.lang3.StringUtils;
+import static com.google.common.base.Preconditions.checkNotNull;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+@Slf4j
 public class HomePage extends BasePage {
 	private static final String URL = StringUtils.EMPTY;
 
+	@FindBy(how = How.ID, using = "search-key")
+	private WebElement searchTextbox;
+
+	@FindBy(how = How.CLASS_NAME, using = "search-button")
+	private WebElement searchButton;
+
+	@FindBy(how = How.CLASS_NAME, using = "close-layer")
+	private WebElement closePopupButton;
+
 	public HomePage() {
 		super();
-		open(this);
+		log.info("Opening Home page");
+		open(this, URL);
+		closePopupIfNecessary(closePopupButton);
+	}
+
+	public SearchResultsPage search(String searchTerm) {
+		checkNotNull(searchTerm, "The loan amount cannot be null");
+		log.info("Entering searchTerm={}", searchTerm);
+		//Validating element is visible before performing operations on it.
+		waitUntil(ExpectedConditions.visibilityOf(searchTextbox));
+		searchTextbox.click();
+		searchTextbox.sendKeys(searchTerm);
+		searchButton.click();
+		return new SearchResultsPage();
 	}
 
 	@Override
